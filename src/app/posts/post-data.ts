13b807b1c09f843c9aa7a7eeb6cd6c53b68db248
Jesus8556 <luis.gutierrez.m@tecsup.edu.pe>
2023-11-22 00:21:45 -0500

    lab12

---
+++ src/app/posts/post-data.ts
@@ -0,0 +1,34 @@
+// post-data.ts
+export const postData = [
+  {
+    id: 1,
+    title: 'Angular acaba de actualizar!!!',
+    content: 'Angular 17 tuvo una actualización asombrosa....',
+    imageUrl: 'https://i0.wp.com/blog.knoldus.com/wp-content/uploads/2020/06/angular.png?fit=1600%2C840&ssl=1',
+    date: '2023-11-20',
+    author: 'Elijah Turner',
+    category: 'Programming',
+    tags: ['Angular.js', 'Programador junir'],
+  },
+  {
+    id: 2,
+    title: 'Python , lo mejor para iniciar !!!',
+    content: 'Python con Django es uno de los frameworks más usados....',
+    imageUrl: 'https://logowik.com/content/uploads/images/python.jpg',
+    date: '2023-11-21',
+    author: 'Scarlett White',
+    category: 'Programming',
+    tags: ['JavaScript', 'Coding'],
+  },
+  {
+    id: 3,
+    title: 'Docker , el rey de los contenedores',
+    content: 'Docker es uno de los programas más usados para los contenedores....',
+    imageUrl: 'https://1000logos.net/wp-content/uploads/2021/11/Docker-Logo-2013.png',
+    date: '2023-11-22',
+    author: 'Lucas Peterson',
+    category: 'Machine Learning',
+    tags: ['Deep Learning', 'Neural Networks'],
+  },
+ 
+];
---
+++ src/app/users/user-data.ts
@@ -0,0 +1,9 @@
+export const userData = [
+  { id: 1, name: 'Elijah Turner', email: 'elijah.turner@example.com', phone: '555-2468' },
+  { id: 2, name: 'Scarlett White', email: 'scarlett.white@example.com', phone: '555-1357' },
+  { id: 3, name: 'Lucas Peterson', email: 'lucas.peterson@example.com', phone: '555-8642' },
+  { id: 4, name: 'Grace Martin', email: 'grace.martin@example.com', phone: '555-7193' },
+  { id: 5, name: 'Caleb Roberts', email: 'caleb.roberts@example.com', phone: '555-3942' },
+  { id: 6, name: 'Zoe Turner', email: 'zoe.turner@example.com', phone: '555-6218' },
+
+];
